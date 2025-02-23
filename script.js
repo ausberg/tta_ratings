b6405a908f552ba077b9cb84cc65ed4ef508872e
ausberg <ausberg4@gmail.com>
2025-02-23 16:00:22 -0600

    Merge from Dev

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ async function loadCSV(filename = "ratings_overall.csv", preservePage = false) {
             let selectedColumns = [
                 columns[0],  // Rank
                 columns[1],  // Rank Δ
+                columns[20], // Title
                 columns[2],  // Player
                 parseFloat(columns[3]).toFixed(0),  // C Rating
                 parseFloat(columns[4]).toFixed(1),  // C R Δ
@@ -67,9 +68,10 @@ async function loadCSV(filename = "ratings_overall.csv", preservePage = false) {
                 parseFloat(columns[15]).toFixed(0),  // Ds
                 parseFloat(columns[17]).toFixed(1),  // W%
                 parseFloat(columns[18]).toFixed(2)   // W% Δ
-            ];          
+            ];           
+        
             return selectedColumns;
-        }).filter(columns => columns.length === 17);
+        }).filter(columns => columns.length === 18);
 
         // Reset sorting state when loading a new dataset
         sortDirection = {};
@@ -95,12 +97,12 @@ async function loadCSV(filename = "ratings_overall.csv", preservePage = false) {
 // Ensure the highlight formatting applies correctly
 function formatColumn(value, index) {
     // Columns where highlight formatting applies
-    const highlightColumns = [1, 4, 6, 9, 11, 13, 16]; // Adjust as needed
+    const highlightColumns = [1, 5, 7, 10, 12, 14, 17]; // Adjust as needed
 
     let num = parseFloat(value);
 
     // Special case: Column 11 should be red **only if the value is greater than 0**
-    if (index === 13 && !isNaN(num) && num > 0) {
+    if (index === 14 && !isNaN(num) && num > 0) {
         return `<td class="red">+${num}</td>`;
     }
 
